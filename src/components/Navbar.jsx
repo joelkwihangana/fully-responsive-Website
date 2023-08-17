5f405c7fbe8e4b6a55b12d5fe6edcdfc82594fc0
joelkwihangana <joelkwijoe@gmail.com>
2023-08-17 06:34:46 +0200

    responsive navs fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justfy-between items-center navbar">
       <img src={logo} alt="HooBank" className="w-[124px] h-[43px]" />
@@ -18,6 +19,36 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      {/* nav for small devices  */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="Menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)} // this way react make sure that it keeps state up to date
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient top-20 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal 
+                cursor-pointer 
+                text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
